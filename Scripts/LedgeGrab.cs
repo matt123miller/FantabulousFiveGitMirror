6aa7bd0f57620ee47cb57eaaf9df26ff759a412b
Tish <tishxd@googlemail.com>
2016-10-06 17:27:56 +0100

    updated ledge grab - less janky.
    Player hits trigger box, freezes character and limits controls to only horizontal axis (only rotates character on the spot at the mo, doesn't move along the edge).
    
    When player presses jump button, player is teleported to the top... that's where we'd put the animation instead?

--- Scripts/LedgeGrab.cs
+++ Scripts/LedgeGrab.cs
@@ -1,24 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class LedgeGrab : MonoBehaviour
 {
 
     private GameObject character = null;
+    private bool onLedge = false;
     private GameObject ledgeCollider = null;
     private GameObject ledgeParent = null;
+    private GameObject mobileJoystick = null;
+    private Joystick joystickScript = null;
+    private Rigidbody characterRigidbody = null;
 
     // Use this for initialization
     void Start()
     {
+
         ledgeParent = gameObject.transform.parent.gameObject;
         ledgeCollider = ledgeParent.transform.GetChild(1).gameObject;
+        mobileJoystick = GameObject.Find("MobileJoystick");
+        joystickScript = mobileJoystick.GetComponent<Joystick>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //if on ledge = true and player presses jump, teleport to top of ledge
+        if (onLedge && CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            //stops player from making further input
+            character.gameObject.GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl>().enabled = false;
+            ////teleports character to top of the box
+            character.transform.position = Vector3.Lerp(character.transform.position, ledgeCollider.transform.position, 5);
+            character.transform.position = new Vector3(character.transform.position.x, character.transform.position.y + 0.5f);
+            resetCharacter();
+        }
     }
 
     void OnTriggerEnter(Collider collider)
@@ -26,12 +43,19 @@ public class LedgeGrab : MonoBehaviour
         if (collider.gameObject.tag == "Player")
         {
             character = collider.gameObject;
+            characterRigidbody = character.GetComponent<Rigidbody>();
         }
 
         if (character != null)
         {
             grabLedge();
         }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        resetCharacter();
+        onLedge = false;
     }
 
 
@@ -43,16 +67,17 @@ public class LedgeGrab : MonoBehaviour
     public void grabLedge()
     {
         //stops character from moving
-        Rigidbody characterRigidbody = character.GetComponent<Rigidbody>();
-        characterRigidbody.isKinematic = true;
-        //stops player from making further input
-        character.gameObject.GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl>().enabled = false;
-        //teleports character to top of the box
-        character.transform.position = Vector3.Lerp(character.transform.position, ledgeCollider.transform.position, 5);
-        character.transform.position = new Vector3(character.transform.position.x, character.transform.position.y + 0.5f);
-        //resets character for further input
-        characterRigidbody.isKinematic = false;
-        character.gameObject.GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl>().enabled = true;
+        //  characterRigidbody.isKinematic = true;
+        characterRigidbody.velocity = Vector3.zero;
+        characterRigidbody.constraints = RigidbodyConstraints.FreezePositionY  | RigidbodyConstraints.FreezeRotation;
+        onLedge = true;
+ 
+    }
 
+    public void resetCharacter()
+    {
+        characterRigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+        joystickScript.axesToUse = Joystick.AxisOption.Both;
+        character.gameObject.GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl>().enabled = true;
     }
 }
