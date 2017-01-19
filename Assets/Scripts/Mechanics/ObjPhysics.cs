9997859d2fc52b6a74c58182b2b7a800c8e244be
Tish <tishxd@googlemail.com>
2017-01-19 13:14:07 +0000

    Added rigidbodies to objects that we want to be able to push! Created a script to push things

---
+++ Assets/Scripts/Mechanics/ObjPhysics.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjPhysics : MonoBehaviour {
+
+    private float pushPower = 2.0f;
+    private Rigidbody body;
+
+    public void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        body = hit.collider.attachedRigidbody;
+
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+
+        if (hit.moveDirection.y < -0.3f)
+        {
+            return;
+        }
+
+        Vector3 pushDirection = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        body.velocity = pushDirection * pushPower;
+            
+     }
+
+}
