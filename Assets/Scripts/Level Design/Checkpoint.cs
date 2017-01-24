b0990c5e62c483f32ab9b7c44a8e9162d4ee8d8c
matt123miller Laptop <matt123miller@hotmail.co.uk>
2017-01-24 16:22:44 +0000

    Begun creating a checkpoint system for loading into the game where you left off between play sessions.

---
+++ Assets/Scripts/Level Design/Checkpoint.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Analytics;
+
+[RequireComponent(typeof(Animator))]
+public class Checkpoint : MonoBehaviour
+{
+    public bool activated = false;
+    public static GameObject[] checkpoints;
+    private Animator animator;
+
+    void Start()
+    {
+        checkpoints = GameObject.FindGameObjectsWithTag("Checkpoint");
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // If the player passes through the checkpoint, we activate it
+        if (other.tag == "Player")
+        {
+            ActivateCheckPoint();
+        }
+    }
+
+    private void ActivateCheckPoint()
+    {
+        // We deactive all checkpoints in the scene
+        foreach (GameObject cp in checkpoints)
+        {
+            cp.GetComponent<Checkpoint>().activated = false;
+            //cp.GetComponent<Animator>().SetBool("Active", false);
+        }
+
+        // We activated this instance of the checkpoint, which would be the one just hit
+        activated = true;
+        //animator.SetBool("Active", true);
+    }
+
+    public static Vector3 GetActiveCheckpointPosition()
+    {
+        // If player die without activate any checkpoint, we will return a default position
+        var result = GameObject.FindWithTag("SpawnPoint").transform.position;
+
+        foreach (var point in checkpoints)
+        {
+            if (point.GetComponent<Checkpoint>().activated)
+            {
+                result = point.transform.position;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    public override string ToString()
+    {
+        string playerprefValue = "";
+
+        // What should it say?
+
+        return playerprefValue;
+    }
+}
