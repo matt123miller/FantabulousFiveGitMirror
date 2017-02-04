869d4ba1d1d35e2d2b81abeb32b3f2084ef75bd6
matt123miller Laptop <matt123miller@hotmail.co.uk>
2017-02-04 16:54:48 +0000

    Removed the evil that is Quatenrions! Now it uses friendly angles and only the z axis is considered because it makes most sense for the user and the UI.

--- Assets/Scripts/DeviceScripts/RopeBalance.cs
+++ Assets/Scripts/DeviceScripts/RopeBalance.cs
@@ -8,83 +8,85 @@ using UnityStandardAssets.Characters.ThirdPerson;
 
 public class RopeBalance : MonoBehaviour
 {
-    private Quaternion _enterRotation;
-    private Quaternion _currentPhoneRotation;
-    private Quaternion _negatePhoneRotation;
-
     private ThirdPersonUserControl _character;
+    private GameObject _dial;
 
-    public float currentAngleDifference;
+    private float _enterZAngle;
+    private float _currentZAngle;
+    private float _inverseAngleThreshold;
+
+    public Transform dialRoot;
     public float angleThreshold = 15f;
-    [Range(0,1)]
+    [Range(0, 1)]
     public float characterSpeeed = 0.7f;
     public bool isBalancing = false;
+
     // Use this for initialization
     void Start()
     {
         _character = FindObjectOfType<ThirdPersonUserControl>();
+        _dial = transform.parent.FindChild("BalanceScreen").gameObject;
+
+        _inverseAngleThreshold = angleThreshold * -1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isBalancing)
+        if (!isBalancing) return;
+
+        // Walk slowly forward, it should be facing the node at the other end.
+        _character.CalculateAndMove(characterSpeeed, 0, false);
+
+        var difference = BalanceAngleDifference();
+
+        dialRoot.rotation = Quaternion.Euler(0, 0, difference);
+
+        if (difference > angleThreshold || difference < _inverseAngleThreshold)
         {
-            // Walk slowly forward, it should be facing the node at the other end.
-            _character.CalculateAndMove(characterSpeeed, 0, false);
-            currentAngleDifference = BalanceDifferenceValue();
+            // What happens when it's too much? We fall off I assume.
 
-            print(currentAngleDifference);
-            if (currentAngleDifference > angleThreshold)
-            {
-                // What happens when it's too much? We fall off I assume.
-
-                // Whatever happens, we still have to end the balance.
-                EndBalance();
-            }
+            // Whatever happens, we still have to end the balance.
+            EndBalance();
         }
-
     }
 
-    public void BeginBalance(Transform thisEnd,Transform otherEnd)
+    public void BeginBalance(Transform thisEnd, Transform otherEnd)
     {
+        print("Begin balance");
+
         isBalancing = true;
+        _dial.SetActive(true);
+
         _character.transform.position = thisEnd.position;
         _character.transform.LookAt(otherEnd);
 
-        print("Begin balance");
-        _enterRotation = DeviceRotation.GetRotation();
+        _enterZAngle = DeviceRotation.GetRotation().eulerAngles.z;
+
         GlobalGameManager.Instance.ToggleUI(false);
     }
 
     public void EndBalance()
     {
         print("End balance");
+
         isBalancing = false;
+        _dial.SetActive(false);
+
         GlobalGameManager.Instance.ToggleUI(true);
     }
 
-    public float BalanceDifferenceValue()
+
+    private float BalanceAngleDifference()
     {
-        _currentPhoneRotation = DeviceRotation.GetRotation();
+        // What is the z angle of the phone? 
+        _currentZAngle = DeviceRotation.GetRotation().eulerAngles.z;
+        // How different is it to the entry z angle?
+        var difference = _enterZAngle - _currentZAngle;
 
-        // Use Quaternion.angle(q1, q2) or Atan2(q1.x, q2.x) ????
-        // What is the difference between entry and current rotations?
-        return Quaternion.Angle(_enterRotation, _currentPhoneRotation);
-        //return Mathf.Atan2(_enterRotation.z, _currentPhoneRotation.z).AsAngle();
+        if (difference > 180) difference -= 360f;
+
+        // Return the inverse as otherwise the UI rotation is counter intuitive (it's the opposite)
+        return difference * -1;
     }
-
-
-
-    //void OnTriggerExit(Collider other)
-    //{
-    //    if (!other.CompareTag("Player")) return;
-
-    //    //other.transform.rotation = transform.localRotation;
-    //    //var pos = other.transform.position;
-    //    //Vector3 posAdjusment = new Vector3(pos.x, pos.y, transform.position.z);
-    //    //other.transform.position = posAdjusment;
-
-
-    //}
 }
