ae229756ff6aa033f852ee8dd5054e329e665cf4
Tish <tishxd@googlemail.com>
2017-02-17 12:47:44 +0000

    Starting work on microphone input, currently testable in Maytham Scene. Character flies upwards with loud noise.

---
+++ Assets/Scripts/DeviceScripts/MicrophoneInput.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class MicrophoneInput : MonoBehaviour
+{
+    public float sensitivity = 100;
+    public float loudness = 0;
+    public AudioSource audioSource;
+    bool started;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void StartInput()
+    {
+        started = true;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Microphone.Start(null, true, 1, 44100);
+        audioSource.loop = true;
+        while (!(Microphone.GetPosition(null) > 0)) { }
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        loudness = GetAveragedVolume() * sensitivity;
+        if (started)
+            print("LOUDNESS: " + loudness);
+    }
+
+    float GetAveragedVolume()
+    {
+        float[] data = new float[256];
+        float a = 0;
+        audioSource.GetOutputData(data, 0);
+        foreach (float num in data)
+        {
+            a += Mathf.Abs(num);
+        }
+        return a / 256;
+    }
+
+    public void StopInput()
+    {
+        if(started)
+        {
+            audioSource.Stop();
+            started = false;
+            loudness = 0;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Mechanics/Floating.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floating : MonoBehaviour {
+
+    GameObject playerObj;
+    Rigidbody playerRigid;
+    MicrophoneInput microphoneInputScript;
+    bool isFloating;
+
+	// Use this for initialization
+	void Start () {
+        playerObj = GameObject.FindGameObjectWithTag("Player");
+        playerRigid = playerObj.GetComponent<Rigidbody>();
+        microphoneInputScript = GameObject.Find("GameManager").GetComponent<MicrophoneInput>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(isFloating)
+        {
+            playerRigid.AddRelativeForce(0, microphoneInputScript.loudness, 0);
+        }
+	}
+
+    public void FloatingTriggered()
+    {
+        isFloating = true;
+        microphoneInputScript.StartInput();
+    }
+
+    public void TurnOffFloating()
+    {
+        isFloating = false;
+        microphoneInputScript.StopInput();
+    }
+}
