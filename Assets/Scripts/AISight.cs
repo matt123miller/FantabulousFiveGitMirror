45c61efb247dd2ff6dab4aba6d07d7ad6ef9c9dd
matt123miller <matt123miller@hotmail.co.uk>
2017-03-23 16:02:30 +0000

    Working on a bit more enemy AI

---
+++ Assets/Scripts/AISight.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    public class AISight : MonoBehaviour
+    {
+        AICharacterControl aiController;
+        RaycastHit hit;
+        private bool inSight;
+        public float updateTarget = 0.2f;
+        public float updateTimer = 0.0f;
+        Vector3 targetPos;
+
+        [SerializeField]
+        private Transform eyes;
+
+        public Transform Eyes
+        {
+            get { return eyes; }
+            set { eyes = value; }
+        }
+
+        private void Start()
+        {
+            // Start each agents timer on a semi random time, why not? 
+            // Should space out the bigger computations across different frames instead of all agents in 1 frame
+            updateTimer += Random.Range(-0.1f, 0.1f);
+        }
+
+
+        void OnTriggerStay(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                updateTimer += Time.deltaTime;
+                if (updateTimer > updateTarget)
+                {
+                    updateTimer = 0.0f;
+                    Look(other.transform.position);
+                    aiController.SetInSight(inSight);
+                }
+            }
+        }
+
+        public bool Look(Vector3 otherPosition)
+        {
+            Vector3 direction = otherPosition - eyes.position;
+
+            hit = new RaycastHit();
+
+            Debug.DrawRay(transform.position, direction, Color.green);
+            if (Physics.Raycast(eyes.position, direction, out hit))
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+                    inSight = true;
+                    Debug.Log("Player is Sighted, player LKP updated");
+                }
+            }
+            else
+            {
+                inSight = false;
+                Debug.Log("In collider, not in sight");
+            }
+            return inSight;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (inSight && other.CompareTag("Player"))
+            {
+                // if this ever occurs and the player IS in sight then there's a problem
+                // if not delete this breakpoint
+                // Breakpoint removed for now, might add it agian later to test.
+                inSight = false;
+                aiController.SetInSight(inSight);
+            }
+        }
+    }
+
+}
--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -3,13 +3,29 @@ using UnityEngine;
 
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
-    [RequireComponent(typeof (NavMeshAgent))]
-    [RequireComponent(typeof (ThirdPersonCharacter))]
+    [RequireComponent(typeof(NavMeshAgent))]
+    [RequireComponent(typeof(ThirdPersonCharacter))]
     public class AICharacterControl : MonoBehaviour
     {
-        public NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
-        public ThirdPersonCharacter character { get; private set; } // the character we are controlling
-        public Transform target;                                    // target to aim for
+        private enum AIState
+        {
+            Patrol = 0,
+            Chase = 1,
+            Attack = 2
+        }
+
+        public NavMeshAgent agent { get; private set; }                 // the navmesh agent required for the path finding
+        public ThirdPersonCharacter character { get; private set; }     // the character we are controlling
+        public ThirdPersonUserControl player { get; private set; }
+        private AISight aiSight;
+
+        [SerializeField]
+        private AIState currentState;
+        public bool inSight = false;
+        public Transform movementTarget;                                // target to aim for
+        public float sightDistance = 15f;
+        public float attackDistance = 5f;
+
         public Transform[] waypoints;
         public int currentWaypoint;
 
@@ -18,26 +34,45 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             // get the components on the object we need ( should not be null due to require component so no need to check )
             agent = GetComponentInChildren<NavMeshAgent>();
             character = GetComponent<ThirdPersonCharacter>();
+            player = FindObjectOfType<ThirdPersonUserControl>();
 
-	        agent.updateRotation = false;
-	        agent.updatePosition = true;
-            target = waypoints[currentWaypoint];
+            agent.updateRotation = false;
+            agent.updatePosition = true;
+
+            currentState = AIState.Patrol;
+            movementTarget = waypoints[currentWaypoint];
 
         }
 
 
         private void Update()
         {
-            //if (target)
-            //    agent.SetDestination(target.position);
-
             if (agent.remainingDistance > agent.stoppingDistance)
                 character.Move(agent.desiredVelocity, false, false, false, Vector3.zero);
             else
                 character.Move(Vector3.zero, false, false, false, Vector3.zero);
 
-            // Waypointing
-            Patrol();
+            // Can it see the player?
+
+
+
+            // Perform state logic.
+            switch (currentState)
+            {
+                case AIState.Patrol:
+                    // Waypointing
+                    Patrol();
+                    break;
+                case AIState.Chase:
+                    Chase(movementTarget);
+                    break;
+                case AIState.Attack:
+                    Attack(movementTarget);
+                    break;
+                default:
+                    break;
+            }
+
         }
 
         private void Patrol()
@@ -45,15 +80,53 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             if (agent.remainingDistance < agent.stoppingDistance)
             {
                 currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
-                target = waypoints[currentWaypoint];
-                agent.SetDestination(target.position);
+                movementTarget = waypoints[currentWaypoint];
+                agent.SetDestination(movementTarget.position);
                 agent.Resume();
             }
         }
 
+        public void Chase(Transform target)
+        {
+            var targetDistance = (target.position - transform.position).magnitude;
+
+            if (inSight && targetDistance > attackDistance)
+            {
+                // Chase the player
+                agent.SetDestination(movementTarget.position);
+                agent.Resume();
+            }
+            else if (inSight && targetDistance < attackDistance)
+            {
+                // Close enough to attack
+                currentState = AIState.Attack;
+                // Do we stop? Don't know
+                agent.Stop();
+
+            }
+            else
+            {
+                currentState = AIState.Patrol;
+            }
+        }
+
+        public void Attack(Transform target)
+        {
+
+        }
+
         public void SetTarget(Transform target)
         {
-            this.target = target;
+            this.movementTarget = target;
+        }
+
+        public void SetInSight(bool value)
+        {
+            inSight = value;
+            if (!value)
+            {
+                currentState = AIState.Patrol;
+            }
         }
     }
 }
