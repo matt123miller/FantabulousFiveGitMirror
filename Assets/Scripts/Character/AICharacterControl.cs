b5a9e3ee3efb40d2cc49631148744de1c11f8960
matt123miller <matt123miller@hotmail.co.uk>
2017-03-31 22:05:47 +0100

    Made the AI patrol, chase the player, stop to attack the player and transition between these states nicely. You can hide behind objects or run away to escape and they'll go back to patrolling. I don't know what all these material changes are, I didn't touch them but oh well.

--- Assets/Scripts/Character/AICharacterControl.cs
+++ Assets/Scripts/Character/AICharacterControl.cs
@@ -15,15 +15,27 @@ public class AICharacterControl : MonoBehaviour
     public NavMeshAgent agent { get; private set; }                 // the navmesh agent required for the path finding
     public ThirdPersonCharacter character { get; private set; }     // the character we are controlling
     public ThirdPersonUserControl player { get; private set; }
-    private AISight aiSight;
+    private AISight _aiSight;
 
     [SerializeField]
-    private AIState currentState;
-    public bool inSight = false;
-    public Transform movementTarget;                                // target to aim for
-    public float sightDistance = 15f;
+    private AIState _currentState;
+    [SerializeField]
+    private bool _inSight = false;
+    [SerializeField]
+    private Transform _movementTarget;                                // target to aim for
+    [Range(0.1f, 0.5f)]
+    public float walkSpeed = 0.4f;
+    [Range(0.5f, 0.8f)]
+    public float chaseSpeed = 0.5f;
+    [Tooltip("Max value of 1 is advised")]
+    [Range(1,1)]
+    public float runSpeed = 1;
+    [Tooltip("How close until the AI attacks?")]
     public float attackDistance = 5f;
+    [Tooltip("How far can they see?")]
+    public float sightDistance = 15f;
 
+    [Header("Waypointing system")]
     public Transform[] waypoints;
     public int currentWaypoint;
 
@@ -33,45 +45,48 @@ public class AICharacterControl : MonoBehaviour
         agent = GetComponentInChildren<NavMeshAgent>();
         character = GetComponent<ThirdPersonCharacter>();
         player = FindObjectOfType<ThirdPersonUserControl>();
-        aiSight = GetComponentInChildren<AISight>();
+        _aiSight = GetComponentInChildren<AISight>();
+        _aiSight.aiController = this;
     }
 
     private void Start()
     {
-        aiSight.aiController = this;
 
         agent.updateRotation = false;
         agent.updatePosition = true;
+        agent.speed = walkSpeed;
 
-        currentState = AIState.Patrol;
-        movementTarget = waypoints[currentWaypoint];
+        _currentState = AIState.Patrol;
+        _movementTarget = waypoints[currentWaypoint];
 
+        // Give the agent a path before anything else happens
+        EnterPatrol();
     }
 
 
     private void Update()
     {
+        // Makes the ThirdPersonCharacter magically work.
         if (agent.remainingDistance > agent.stoppingDistance)
             character.Move(agent.desiredVelocity, false, false, false, Vector3.zero);
         else
             character.Move(Vector3.zero, false, false, false, Vector3.zero);
+        
 
-        // Can it see the player?
-
-
+        var targetDistance = (_movementTarget.position - transform.position).magnitude;
 
         // Perform state logic.
-        switch (currentState)
+        switch (_currentState)
         {
             case AIState.Patrol:
                 // Waypointing
-                Patrol();
+                Patrol(targetDistance);
                 break;
             case AIState.Chase:
-                Chase(movementTarget);
+                Chase(_movementTarget, targetDistance);
                 break;
             case AIState.Attack:
-                Attack(movementTarget);
+                Attack(_movementTarget, targetDistance);
                 break;
             default:
                 break;
@@ -79,57 +94,109 @@ public class AICharacterControl : MonoBehaviour
 
     }
 
-    private void Patrol()
+    private void EnterPatrol()
     {
-        if (agent.remainingDistance < agent.stoppingDistance)
+        _currentState = AIState.Patrol;
+        agent.speed = walkSpeed;
+        SetTarget(waypoints[currentWaypoint]);
+        agent.SetDestination(_movementTarget.position);
+        agent.Resume();
+    }
+
+    private void EnterChase()
+    {
+        _currentState = AIState.Chase;
+        agent.speed = chaseSpeed;
+    }
+
+    private void EnterAttack()
+    {
+        _currentState = AIState.Attack;
+        agent.speed = runSpeed;
+    }
+
+    private void Patrol(float targetdistance)
+    {
+        // Should we exit patrol state into chasing?
+        if (_inSight && targetdistance < sightDistance)
+        {
+            // Close enough to chase
+            EnterChase();
+            // Do we stop? Don't know
+            print("Close enough ot Chase! Changing state!");
+        }
+        // If not lets just patrol more
+        else if (agent.remainingDistance < agent.stoppingDistance)
         {
             currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
-            movementTarget = waypoints[currentWaypoint];
-            agent.SetDestination(movementTarget.position);
+            _movementTarget = waypoints[currentWaypoint];
+            agent.SetDestination(_movementTarget.position);
             agent.Resume();
         }
     }
 
-    public void Chase(Transform target)
+    public void Chase(Transform target, float targetdistance)
     {
-        var targetDistance = (target.position - transform.position).magnitude;
-
-        if (inSight && targetDistance > attackDistance)
+        if (_inSight && targetdistance > attackDistance)
         {
             // Chase the player
-            agent.SetDestination(movementTarget.position);
+            agent.SetDestination(_movementTarget.position);
             agent.Resume();
+            print("Chasing!");
         }
-        else if (inSight && targetDistance < attackDistance)
+        else if (_inSight && targetdistance < attackDistance)
         {
             // Close enough to attack
-            currentState = AIState.Attack;
+            EnterAttack();
             // Do we stop? Don't know
-            agent.Stop();
+            print("Close enough to Attack! Changing state!");
 
         }
         else
         {
-            currentState = AIState.Patrol;
+            print("The player escaped! Changing to Patrol!");
+            EnterPatrol();
         }
     }
 
-    public void Attack(Transform target)
+    public void Attack(Transform target, float targetdistance)
     {
-
+        // Can we see the player?
+        if (!_inSight)
+        {
+            EnterChase();
+            // Do we need to do anything about knowing WHERE to chase them? Or will it all gracefully follow through?
+            // We'll just let chase decide.
+        }
+        // Is the player running away? But I definitely can see them
+        else if (targetdistance > attackDistance)
+        {
+            _currentState = AIState.Chase;
+        }
+        // If everything is good them lets get 'em!
+        else
+        {
+            print("Attacking");
+            agent.Stop(); // Stop so we can do some attack animations?
+            // Now what?
+        }
     }
 
     public void SetTarget(Transform target)
     {
-        this.movementTarget = target;
+        this._movementTarget = target;
     }
 
-    public void SetInSight(bool value)
+    public void SetInSight(bool value, Transform target)
     {
-        inSight = value;
-        if (!value)
+        _inSight = value;
+        if (value)
         {
-            currentState = AIState.Patrol;
+            SetTarget(target);
+        }
+        else
+        {
+            EnterPatrol();
         }
     }
 }
--- Assets/Scripts/Character/AISight.cs
+++ Assets/Scripts/Character/AISight.cs
@@ -8,9 +8,11 @@ public class AISight : MonoBehaviour
     public AICharacterControl aiController;
     RaycastHit _hit;
 
+    [SerializeField]
     private bool _inSight;
     public float updateTarget = 0.1f;
     public float updateTimer = 0.0f;
+    public Collider blockingcollider;
 
     [SerializeField]
     private Transform eyes;
@@ -23,41 +25,52 @@ public class AISight : MonoBehaviour
 
     private void Start()
     {
+        blockingcollider = aiController.GetComponent<Collider>();
         // Start each agents timer on a semi random time, why not? 
         // Should space out the bigger computations across different frames instead of all agents in 1 frame
-        updateTimer += UnityEngine.Random.Range(-0.1f, 0.1f);
+        updateTimer = UnityEngine.Random.Range(-0.2f, 0);
     }
 
-
+    private void Update()
+    {
+        if(updateTimer < 0) 
+            Debug.DrawRay(eyes.position, (transform.position - eyes.position) * 2, Color.green);
+    }
+    
     void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            var target = other.transform.position;
-            target.Set(target.x, target.y + 0.3f, target.z);
-            var direction =  target - eyes.position;
+            var target = other.transform;
+            var targetPos = target.position;
 
-            Debug.DrawRay(eyes.position, direction, Color.red, 0.3f);
+            targetPos.Set(targetPos.x, targetPos.y/* + 0.3f*/, targetPos.z); // I think I'm adding 0.3 to make sure I hit the players body? Probably unnecessary.
+            var direction = targetPos - eyes.position;
+
+            Debug.DrawRay(eyes.position, direction, Color.red);
 
             updateTimer += Time.deltaTime;
             if (updateTimer > updateTarget)
             {
                 updateTimer = 0.0f;
                 _inSight = Look(target, direction);
-                aiController.SetInSight(_inSight);
+                aiController.SetInSight(_inSight, other.transform);
             }
         }
     }
 
-    public bool Look(Vector3 otherPosition, Vector3 direction)
+    public bool Look(Transform target, Vector3 direction)
     {
-        _hit = new RaycastHit();
         var spotted = false;
+        _hit = new RaycastHit();
+        blockingcollider.enabled = false;
 
         if (Physics.Raycast(eyes.position, direction, out _hit))
+            print("Hit something" + _hit.transform.name);
             spotted = _hit.collider.CompareTag("Player");
 
         print(spotted ? "Player is sighted" : "In collider, not in sight");
+        blockingcollider.enabled = true;
 
         return spotted;
     }
@@ -68,7 +81,8 @@ public class AISight : MonoBehaviour
         {
             // The player ran away further than the AI can see
             _inSight = false;
-            aiController.SetInSight(_inSight);
+            aiController.SetInSight(_inSight, other.transform);
+            updateTimer = UnityEngine.Random.Range(-0.2f, 0); ;
         }
     }
 }
