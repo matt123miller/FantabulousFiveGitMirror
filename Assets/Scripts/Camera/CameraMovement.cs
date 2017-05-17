c8e5ca2f70280bbf6f2e7e79749505f1b0fc4064
matt123miller Laptop <matt123miller@hotmail.co.uk>
2017-05-17 18:28:34 +0100

    Little camera tweak for Tish and for some reason a load of Ana's assets say they're new on my end. dunno.

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class CameraMovement : MonoBehaviour {
 
     private ThirdPersonUserControl _thirdPersonUserControl;
     private RopeBalance _balance;
+	private Joystick _joystick;
     private GameObject _character;
     private Rigidbody _characterRigidbody;
     private Vector3 _hangingCameraPos;
@@ -25,37 +27,57 @@ public class CameraMovement : MonoBehaviour {
         _balance = FindObjectOfType<RopeBalance>();
 
     }
+
+	void Start() {
+
+		_joystick = FindObjectOfType<Joystick> ();
+		if (_joystick == null) {
+			Destroy (this);
+		}
+	}
     // Update is called once per frame
     void Update()
     {
-        float characterVelocity = _characterRigidbody.velocity.normalized.magnitude;
+		// This is the old way, relying on player speed to power the lerping.2
+        //float characterVelocity = _characterRigidbody.velocity.normalized.magnitude;
+
+		float characterVelocity = _joystick.movementAmount;
+
         // Change the camera to the 'balancing' position - far out and above
         if (_balance.isBalancing)
         {
-            lerpCameraBetweenPos(characterCam.transform, balancingCameraPos.transform, lerpTime);
+			lerpCameraBetweenPos(idleCameraPos.transform, balancingCameraPos.transform, characterVelocity);
         }
         //changes camera to 'travelling' position - far out
         else if (characterVelocity > 0.1 && characterCam.transform.localPosition != travellingCameraPos.transform.localPosition)
         {
-            lerpCameraBetweenPos(characterCam.transform, travellingCameraPos.transform, lerpTime);
+			lerpCameraBetweenPos(idleCameraPos.transform, travellingCameraPos.transform, characterVelocity);
         }
         //changed camera to 'idle' position - close up
         else if (characterVelocity <= 0.1 && characterCam.transform.localPosition != idleCameraPos.transform.localPosition && !_thirdPersonUserControl.isHanging)
         {
-            lerpCameraBetweenPos(characterCam.transform, idleCameraPos.transform, lerpTime);
+			lerpCameraBetweenPos(idleCameraPos.transform, characterCam.transform, characterVelocity);
         }
         //changes camera to 'travelling' position only if hanging on a ledge
         else if(_thirdPersonUserControl.isHanging)
         {
-            lerpCameraBetweenPos(characterCam.transform, travellingCameraPos.transform, lerpTime);
+			lerpCameraBetweenPos(idleCameraPos.transform, travellingCameraPos.transform, characterVelocity);
         }
 
         print("Velocity:" + characterVelocity);
     }
 
+	/*
     void lerpCameraBetweenPos(Transform originalPos, Transform newPos, float time)
     {
        characterCam.transform.localPosition =  Vector3.Lerp(originalPos.transform.localPosition, newPos.transform.localPosition, Time.deltaTime * time);
        characterCam.transform.localRotation =  Quaternion.Lerp(originalPos.localRotation, newPos.localRotation, Time.deltaTime * time);
     }
+*/
+
+	void lerpCameraBetweenPos(Transform originalPos, Transform newPos, float lerpAmount)
+	{
+		characterCam.transform.localPosition =  Vector3.Lerp(originalPos.transform.localPosition, newPos.transform.localPosition, lerpAmount);
+		characterCam.transform.localRotation =  Quaternion.Lerp(originalPos.localRotation, newPos.localRotation, lerpAmount);
+	}
 }
